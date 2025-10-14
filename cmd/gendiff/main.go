67c4283537fca9f6ab364dc0d89dc52c932fb19a
Mikhail Kuklin <m.a.kuklin@tinkoff.ru>
2025-10-14 08:44:33 +0300

    feat: добавлена поддержка формата вывода в gendiff - добавлен флаг --format (-f) с значением по умолчанию "stylish"

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -19,6 +19,14 @@ func main() {
 		Name:   "gendiff",
 		Usage:  "Compares two configuration files and shows a difference.",
 		Action: GenDiff,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Value:   "stylish",
+				Usage:   "output format",
+				Aliases: []string{"f"},
+			},
+		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
